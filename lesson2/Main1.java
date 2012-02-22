488497195cb09119aec65682c5098bdff494a862
Ilya Varlamov <prevratnic@gmail.com>
2012-02-22 17:02:49 +0400

    add new function lesson 2

--- lesson2/Main1.java
+++ lesson2/Main1.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
 public class Main1 {
     public static void main( String[] args ){
 
+/*
         System.out.println("Введите колличество строк матрицы :");
 
         Scanner scanner = new Scanner( System.in );
@@ -23,15 +24,42 @@ public class Main1 {
         int n = scanner.nextInt();
 
         Matrix matrix = new Matrix( m , n );
-        matrix.addItem( 1 );
-        matrix.addItem( 2 );
-        matrix.addItem( 3 );
-        matrix.addItem( 4 );
-        matrix.addItem( 5 );
-        matrix.addItem( 6 );
-        matrix.addItem( 7 );
 
-        matrix.output();
+        System.out.println("Введите элементы матрицы :");
+
+        for( int i = 0; i < m; i++ ){
+            for( int j = 0; j < n; j++ ){
+                matrix.addItem( scanner.nextInt() );
+            }
+        }
+*/
+        Matrix matrix_1 = new Matrix( 2, 3 );
+        matrix_1.addItem( 1 );
+        matrix_1.addItem( 2 );
+        matrix_1.addItem( 3 );
+        matrix_1.addItem( 1 );
+        matrix_1.addItem( 2 );
+        matrix_1.addItem( 1 );
+
+        Matrix matrix_2 = new Matrix( 2, 3 );
+        matrix_2.addItem( 1 );
+        matrix_2.addItem( 2 );
+        matrix_2.addItem( 3 );
+        matrix_2.addItem( 1 );
+        matrix_2.addItem( 2 );
+        matrix_2.addItem( 1 );
+
+        try{
+/*
+            Matrix out = matrix_1.add( matrix_2 );
+            out.output();
+*/
+
+            System.out.print( matrix_1.equal( matrix_2 ) );
+
+        } catch ( Exception e ){
+            e.printStackTrace();
+        }
 
     }
 }
--- lesson2/contener/Matrix.java
+++ lesson2/contener/Matrix.java
@@ -12,6 +12,9 @@ public class Matrix {
     private int m;
     private int n;
 
+    private int i;
+    private int j;
+
     public Matrix( int m, int n ){
         this.m = m;
         this.n = n;
@@ -19,18 +22,61 @@ public class Matrix {
         this.matrix = new int[ m ][ n ];
     }
     
+    public void addItem( int item, int m, int n ){
+        matrix[m][n] = item;
+    }
+
     public void addItem( int item ){
+        if( j < n ){
+            matrix[i][j] = item;
+            j++;
+        }
+        if( j == n ){
+            j = 0;
+            i++;
+        }
+    }
+
+    public Matrix add( Matrix sumMatrix ) throws Exception {
+
+        Matrix result = null;
+
+        if( (m == sumMatrix.m) && (n == sumMatrix.n) ){
+            result = new Matrix( m, n );
+
+            for( int i = 0; i < m; i++ ){
+                for( int j = 0; j < n; j++ ){
+                    result.addItem( (this.matrix[i][j]) + (sumMatrix.matrix[i][j]) );
+                }
+            }
+
+        }else {
+            throw new Exception("Error matrix size ");
+        }
+
+        return result;
+    }
+
+    public boolean equal( Matrix eqMatrix ){
+
+        boolean flag = false;
+
+        if( (m != eqMatrix.m) || (n != eqMatrix.n) ) return false;
+
         for( int i = 0; i < m; i++ ){
             for( int j = 0; j < n; j++ ){
-                matrix[i][j] = item;
+                flag = this.matrix[i][j] == eqMatrix.matrix[i][j];
+                if( flag == false ) return false;
             }
         }
+
+        return flag;
     }
 
     public void output(){
         for( int i = 0; i < m; i++ ){
             for( int j = 0; j < n; j++ ){
-                System.out.print( matrix[i][j] + " ");
+                System.out.print( matrix[i][j] + "\t");
             }
             System.out.println("\n");
         }
